685986203bad1f6e88c457024c73fd91f8407944
vincent-baret-974 <vincentbaretcontact@gmail.com>
2024-01-03 21:01:24 +0100

    feat: change app layout and create logic for pages navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_book_app/pages/AccountPage.dart';
+import 'package:food_book_app/pages/CartPage.dart';
+import 'package:food_book_app/pages/ChatPage.dart';
+import 'package:food_book_app/pages/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,32 +14,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Stack(
-            children: [
-              BottomTabBar()
-            ],
-        ),
-      )
+        debugShowCheckedModeBanner: false,
+        home: AppLayout()
     );
   }
 }
 
-class BottomTabBar extends StatefulWidget {
-  const BottomTabBar({super.key});
+class AppLayout extends StatefulWidget {
+  const AppLayout({super.key});
 
   @override
-  State<BottomTabBar> createState() => _BottomTabBarState();
+  State<AppLayout> createState() => _AppLayoutState();
 }
 
-class _BottomTabBarState extends State<BottomTabBar> {
-  Color homeIconColor = Colors.grey;
+class _AppLayoutState extends State<AppLayout> {
+  Color homeIconColor = Colors.white;
   Color chatIconColor = Colors.grey;
   Color cartIconColor = Colors.grey;
   Color accountIconColor = Colors.grey;
 
-  String selected = "";
+  String selected = "home";
+  Widget currentPage = const HomePage();
 
   void resetColorPreviouslySelectedIcon(String previous) {
     switch (previous) {
@@ -52,82 +51,86 @@ class _BottomTabBarState extends State<BottomTabBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-        bottom: 20,
-        left: 0,
-        right: 0,
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            width: 350,
-            decoration: const BoxDecoration(
+    return Scaffold(
+        body: Center(
+            child: currentPage
+        ),
+        bottomNavigationBar:
+        Container(
+          margin: const EdgeInsets.fromLTRB(20, 0, 20, 30),
+          padding: const EdgeInsets.all(20),
+          width: 350,
+          decoration: const BoxDecoration(
               color: Colors.black,
               borderRadius: BorderRadius.all(Radius.circular(40))
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                    onPressed: () {
-                      setState(() {
-                        resetColorPreviouslySelectedIcon(selected);
-                        homeIconColor = Colors.white;
-                        selected = "home";
-                      });
-                    },
-                    icon: Icon(
-                        Icons.home_filled,
-                        color: homeIconColor,
-                        size: 25
-                    ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    resetColorPreviouslySelectedIcon(selected);
+                    homeIconColor = Colors.white;
+                    selected = "home";
+                    currentPage = const HomePage();
+                  });
+                },
+                icon: Icon(
+                    Icons.home_filled,
+                    color: homeIconColor,
+                    size: 25
                 ),
-                const SizedBox(width: 10),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      resetColorPreviouslySelectedIcon(selected);
-                      chatIconColor = Colors.white;
-                      selected = "chat";
-                    });
-                  },
-                  icon: Icon(
-                      Icons.chat_rounded,
-                      color: chatIconColor,
-                      size: 25
-                  ),
+              ),
+              const SizedBox(width: 10),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    resetColorPreviouslySelectedIcon(selected);
+                    chatIconColor = Colors.white;
+                    selected = "chat";
+                    currentPage = const ChatPage();
+                  });
+                },
+                icon: Icon(
+                    Icons.chat_rounded,
+                    color: chatIconColor,
+                    size: 25
                 ),
-                const SizedBox(width: 10),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      resetColorPreviouslySelectedIcon(selected);
-                      cartIconColor = Colors.white;
-                      selected = "cart";
-                    });
-                  },
-                  icon: Icon(
-                      Icons.shopping_cart_rounded,
-                      color: cartIconColor,
-                      size: 25
-                  ),
+              ),
+              const SizedBox(width: 10),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    resetColorPreviouslySelectedIcon(selected);
+                    cartIconColor = Colors.white;
+                    selected = "cart";
+                    currentPage = const CartPage();
+                  });
+                },
+                icon: Icon(
+                    Icons.shopping_cart_rounded,
+                    color: cartIconColor,
+                    size: 25
                 ),
-                const SizedBox(width: 10),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      resetColorPreviouslySelectedIcon(selected);
-                      accountIconColor = Colors.white;
-                      selected = "account";
-                    });
-                  },
-                  icon: Icon(
-                      Icons.account_circle_rounded,
-                      color: accountIconColor,
-                      size: 25
-                  ),
+              ),
+              const SizedBox(width: 10),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    resetColorPreviouslySelectedIcon(selected);
+                    accountIconColor = Colors.white;
+                    selected = "account";
+                    currentPage = const AccountPage();
+                  });
+                },
+                icon: Icon(
+                    Icons.account_circle_rounded,
+                    color: accountIconColor,
+                    size: 25
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         )
     );
