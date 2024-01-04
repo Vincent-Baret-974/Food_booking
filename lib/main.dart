a4f12684cdb64872f0503cfeceae750121b72041
vincent-baret-974 <vincentbaretcontact@gmail.com>
2024-01-04 21:09:33 +0100

    feat: add custom appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:food_book_app/pages/AccountPage.dart';
 import 'package:food_book_app/pages/CartPage.dart';
 import 'package:food_book_app/pages/ChatPage.dart';
@@ -52,9 +55,94 @@ class _AppLayoutState extends State<AppLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: currentPage
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(100),
+          child: SafeArea(
+            child: Container(
+              margin: const EdgeInsets.only(top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      border: Border.all(
+                        color: Colors.grey.shade400,
+                        width: 1
+                      )
+                    ),
+                    padding: const EdgeInsets.all(2),
+                    child: IconButton(
+                      highlightColor: Colors.transparent,
+                      icon: Icon(
+                          Icons.search_rounded,
+                          color: Colors.grey.shade600,
+                          size: 35
+                      ),
+                      onPressed: () {
+
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Column(
+                      children: [
+                        Text(
+                            'Current location',
+                            style: GoogleFonts.poppins(
+                              fontSize: 14,
+                              color: Colors.grey
+                            ),
+                        ),
+                        Row(
+                          children: [
+                            const Icon(
+                                Icons.place,
+                                color: Colors.green,
+                                size: 20
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                                'Saint-Joseph, Réunion',
+                                style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600
+                                ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(Radius.circular(10)),
+                        border: Border.all(
+                            color: Colors.grey.shade400,
+                            width: 1
+                        )
+                    ),
+                    padding: const EdgeInsets.all(2),
+                    child: IconButton(
+                      highlightColor: Colors.transparent,
+                      icon: const Icon(
+                          Icons.notifications_rounded,
+                          color: Colors.black,
+                          size: 35
+                      ),
+                      onPressed: () {
+
+                      },
+                    ),
+                  ),
+                ],
+              )
+            ),
+          )
         ),
+        body: currentPage,
         bottomNavigationBar:
         Container(
           margin: const EdgeInsets.fromLTRB(20, 0, 20, 30),
