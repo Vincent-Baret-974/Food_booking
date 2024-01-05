ad98574145ca45c39ce2c9076f0209999489949e
vincent-baret-974 <vincentbaretcontact@gmail.com>
2024-01-05 17:55:14 +0100

    feat: finish implementation of OrderCard

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,42 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/OrderCard.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  SingleChildScrollView(
-      child: Container(
-        padding: const EdgeInsets.all(40),
-        margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey, // Shadow color with opacity
-              spreadRadius: 1, // Spread radius
-              blurRadius: 1, // Blur radius
-              offset: Offset(0, 2), // Offset for the shadow
-            ),
-          ],
-        ),
-        child: Row(
-          children: [
-            const Text('Claim your daily\n free delivery now!'),
-            Expanded(
-              child: Container(
-                height: 150,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('images/illustration_home_page.jpg'),
-                      fit: BoxFit.contain
-                  )
-                ),
-              ),
-            ),
-          ],
-        ),
+    return  const SingleChildScrollView(
+      child: Column(
+        children: [
+          OrderCard(),
+        ],
       ),
     );
   }
---
+++ lib/widgets/OrderCard.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class OrderCard extends StatelessWidget {
+  const OrderCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey, // Shadow color with opacity
+            spreadRadius: 1, // Spread radius
+            blurRadius: 1, // Blur radius
+            offset: Offset(0, 1), // Offset for the shadow
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Claim your daily\nfree delivery now!',
+                style: GoogleFonts.poppins(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w600
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black
+                  ),
+                  child: Text(
+                      'Order now',
+                      style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600
+                      )
+                  ),
+                  onPressed: () {
+
+                  }
+              )
+            ],
+          ),
+          const SizedBox(width: 30),
+          Expanded(
+            child: Container(
+              height: 140,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/illustration_home_page.jpg'),
+                      fit: BoxFit.contain
+                  )
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
